20a3e0cec35ee582db27b48ef35fa2e6515543a2
Josue Roque <josueroque@yahoo.com>
2023-06-14 13:59:48 -0600

    Modify exercise 1

--- 02-lenguajes/ejercicio1.ts
+++ 02-lenguajes/ejercicio1.ts
@@ -1,26 +1,26 @@
 //Ejercicio 1
 
-var head = (array: unknown[]): unknown => {
+const head = (array: unknown[]): unknown => {
   const [first] = array;
   return first;
 };
 
 console.log(head([5, 2, 4]));
 
-var tail = ([first, ...rest]: unknown[]) => {
+const tail = ([first, ...rest]: unknown[]) => {
   return rest;
 };
 
 console.log(tail([5, 2, 4]));
 
-var init = (array: unknown[]) => {
+const init = (array: unknown[]) => {
   const modified = array.slice(0, array.length - 1);
   return modified;
 };
 
 console.log(init([5, 2, 4]));
 
-var last = (array: unknown[]) => {
+const last = (array: unknown[]) => {
   return array[array.length - 1];
 };
 
