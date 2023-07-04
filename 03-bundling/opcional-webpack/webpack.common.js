a7f96900358f032a589304c46de492786e48f218
Josue Roque <josueroque@yahoo.com>
2023-07-04 17:32:44 -0600

    Add webpack config files

--- 03-bundling/opcional-webpack/webpack.common.js
+++ 03-bundling/opcional-webpack/webpack.common.js
@@ -1,30 +1,25 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const path = require("path");
 
 module.exports = {
   context: path.resolve(__dirname, "./src"),
   resolve: {
-    extensions: [".js", ".ts"],
+    extensions: [".js", ".ts", ".tsx"],
   },
   entry: {
-    app: "./index.ts",
+    app: "./index.tsx",
   },
   output: {
+    filename: "[name].[chunkhash].js",
     path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
       {
-        test: /\.ts$/,
+        test: /\.tsx?$/,
         exclude: /node_modules/,
         loader: "babel-loader",
-      },
-      {
-        test: /\.scss$/,
-        exclude: /node_modules/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
       {
         test: /\.(png|jpg)$/,
@@ -37,18 +32,12 @@ module.exports = {
     ],
   },
   plugins: [
+    //Generate index.html in /dist => https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: "./index.html",
-      scriptLoading: "blocking",
-    }),
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css",
+      filename: "index.html", //Name of file in ./dist/
+      template: "./index.html", //Name of template in ./src
+      scriptLoading: "blocking", // Just use the blocking approach (no modern defer or module)
     }),
     new CleanWebpackPlugin(),
   ],
-  devServer: {
-    port: 8080,
-  },
 };
