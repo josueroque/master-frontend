1c81d29d64b8bacd642ff7790a52d2fa63d77dc2
Josue Roque <josueroque@yahoo.com>
2023-10-10 14:03:26 -0600

    Add menu and required components

--- 04-frameworks/angular/basico/src/app/app.module.ts
+++ 04-frameworks/angular/basico/src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuComponent } from './layout/public/menu/menu.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { GaleriaComponent } from './galeria/galeria.component';
+import { CrudComponent } from './crud/crud.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    LoginComponent,
+    AboutComponent,
+    DashboardComponent,
+    GaleriaComponent,
+    CrudComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
