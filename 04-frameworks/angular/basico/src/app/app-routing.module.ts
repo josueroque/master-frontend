2fa24c0cd38fd3d77c0b16e8cc374fdee3bf01b4
Josue Roque <josueroque@yahoo.com>
2023-10-17 09:43:39 -0600

    Add comoponents routes

--- 04-frameworks/angular/basico/src/app/app-routing.module.ts
+++ 04-frameworks/angular/basico/src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { GalleryComponent } from './gallery/gallery.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CrudComponent } from './crud/crud.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'crud', component: CrudComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'gallery', component: GalleryComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
